066e91cbd990a529c47bac62eafcee62229f6f78
洪荣钦 <15489306+hongrongqin@user.noreply.gitee.com>
2025-06-23 14:58:52 +0800

    删导航

--- djangoproject/navigation/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Navigation
-
-
-class NavigationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Navigation
-        fields = '__all__'
--- djangoproject/navigation/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import GetNavigationAPIView
-
-app_name = 'article'
-
-urlpatterns = [
-    path('get', GetNavigationAPIView.as_view(), name='get_navigation'),
-]
